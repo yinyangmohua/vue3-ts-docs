8c977b8df98d343cb86dcdf5ae857f3109db20d7
yinyangmohua <3259296522@qq.com>
2024-10-08 18:37:48 +0800

    DOC学习

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
     themeConfig: {
-        logo: "/assets/logo.png",
+        logo: "https://camo.githubusercontent.com/03a38e35c1405d4c6d043f9f0464665098c227a5a1f19c36a5d928a6d7558975/68747470733a2f2f70392d6a75656a696e2e62797465696d672e636f6d2f746f732d636e2d692d6b3375316662706663702f66666433653233386565376634366561623432626638386166313766353532387e74706c762d6b3375316662706663702d696d6167652e696d616765233f773d323526683d323626733d3539363826653d73766726613d3126623d646163626263",
         nav: [
             { text: "首页", link: "/"},
             { text: "笔记", link: "/components/"},
